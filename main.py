e96c2cd5a109c0171cb1996e6c52b11ed1330da6
Gnanesh Balusa <98212179+gnanesh-16@users.noreply.github.com>
2023-12-04 14:31:54 +0530

    Add Main-api

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ async def create_user(user: UserBase,db: db_dependency):
 async def read_user(user_id:int,db: db_dependency):
     user = db.query(models.User).filter(models.User.id == user_id).first()
     if user is None:
-        raise HTTPException(status_code=404,detail='User not found')
+        raise HTTPException(status_code=200,detail='User not found')
     return  user
 
 @app.delete("/users/{user_id}", status_code=status.HTTP_200_OK)
